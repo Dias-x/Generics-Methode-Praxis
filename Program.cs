d03af720dab0ece38aea003d0715696c482f20d6
Dias-x <andre.d14x@gmail.com>
2025-01-06 00:14:00 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Liste erstellen
+        List<string> namensListe = new List<string>();
+        AddMultiple<string>(namensListe, 5, "Dias");
+        
+        
+        foreach(string name in namensListe)
+            Console.WriteLine(name);
+    }
+
+    
+    //Generics Methode erstellen mit den Werten
+    static void AddMultiple<T>(List<T> list, int amount, T value)
+    {
+        //Wert solange zur Liste hinzufügen, bis der amount "5" durchgeführt wurde
+        for (int i = 0; i < amount; i++)
+        {
+            list.Add(value);
+        }
+    }
+}
